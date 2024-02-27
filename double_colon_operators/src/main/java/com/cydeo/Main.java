86669ee9769c635b8c2910962668f528f3504703
omrzcn <omrzcnn@gmail.com>
2024-02-27 10:01:39 +0300

    classes created

---
+++ double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Calculate sum = (x,y) -> System.out.println(x+y);
+        Calculate sum1 =(x,y)-> Calculator.findSum(x,y); // maybe someone created this method. we can use it instead of above code.
+
+
+
+
+
+    }
+}
